0d3543da2365273f35131f17c68bf623ee3b45d1
John McMeen <johnmcmeen@gmail.com>
2026-03-28 19:25:57 -0400

    Update 04-functions.sql

--- db/init/04-functions.sql
+++ db/init/04-functions.sql
@@ -139,8 +139,10 @@ AS $$
         count(o.observation_uuid) AS observation_count
     FROM taxa t
     LEFT JOIN observations o ON o.taxon_id = t.taxon_id
-    WHERE t.ancestry LIKE '%/' || parent_id::text
-       OR t.ancestry = parent_id::text
+    WHERE split_part(
+              t.ancestry, '/',
+              array_length(string_to_array(t.ancestry, '/'), 1)
+          ) = parent_id::text
     GROUP BY t.taxon_id, t.name, t.rank, t.rank_level
     ORDER BY observation_count DESC
     LIMIT lim;
